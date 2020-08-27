abd92f9fe40d36146952260b50b7dc9506320d76
Bruno Bernardi <brunobbf84@gmail.com>
2020-08-27 18:35:37 -0300

    Solution with separate Sets of Students and addAll method

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,40 +4,42 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
+import entities.Student;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		Set<Integer> a = new HashSet<>();
-		Set<Integer> b = new HashSet<>();
-		Set<Integer> c = new HashSet<>();
+		Set<Student> courseA = new HashSet<>();
+		Set<Student> courseB = new HashSet<>();
+		Set<Student> courseC = new HashSet<>();
 		
 		System.out.print("How many students for course A? ");
 		int n = sc.nextInt();
 		for (int i = 0; i < n; i++) {
-			int number = sc.nextInt();
-			a.add(number);
+			Integer id = sc.nextInt();
+			courseA.add(new Student(id));
 		}
 		
 		System.out.print("How many students for course B? ");
 		n = sc.nextInt();
 		for (int i = 0; i < n; i++) {
-			int number = sc.nextInt();
-			b.add(number);
+			Integer id = sc.nextInt();
+			courseB.add(new Student(id));
 		}
 		
 		System.out.print("How many students for course C? ");
 		n = sc.nextInt();
 		for (int i = 0; i < n; i++) {
-			int number = sc.nextInt();
-			c.add(number);
+			Integer id = sc.nextInt();
+			courseC.add(new Student(id));
 		}
 		
-		Set<Integer> total = new HashSet<>(a);
-		total.addAll(b);
-		total.addAll(c);
+		Set<Student> total = new HashSet<>(courseA);
+		total.addAll(courseB);
+		total.addAll(courseC);
 		
 		System.out.println("Total students: " + total.size());
 		
